21f4e5f9b5c285012bb1932e0bbdb00739404ec3
rmalladi-sf <131613346+rmalladi-sf@users.noreply.github.com>
2023-04-30 17:40:39 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,6 +32,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Display Normalized Response
 streamlit.dataframe(fruityvice_normalized)
+streamlit.stop()
 
 import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
